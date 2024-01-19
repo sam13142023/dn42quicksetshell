4fb9eadd3a73cc17671c7bfbbf3ab17c25a303c3
sam <65808531+sam13142023@users.noreply.github.com>
2024-01-19 21:24:07 +0800

    bash.py update

--- bash.py
+++ bash.py
@@ -1,5 +1,16 @@
 import requests
 import subprocess
+import yaml
+import os
+
+def get_git_commit_hash():
+    try:
+        # 获取本地 Git 仓库的最新 commit hash
+        commit_hash = subprocess.check_output(['git', 'rev-parse', 'HEAD']).decode('utf-8').strip()
+        return commit_hash
+    except subprocess.CalledProcessError:
+        # 如果发生错误（例如不在Git仓库中），返回默认字符串
+        return "Unknown"
 
 def update_roa_script():
     url = 'https://sam1314.com/dn42/upgraderoa.sh'
@@ -56,12 +67,55 @@ AllowedIPs = 10.0.0.0/8, 172.20.0.0/14, 172.31.0.0/16, fd00::/8, fe80::/64
     subprocess.run(['sudo', 'birdc', 'configure'])
     subprocess.run(['sudo', 'birdc', 'show', 'protocol'])
 
+def save_node_info():
+    print("保存你的节点信息")
+
+    # 二级菜单
+    print("\n请选择相关功能:")
+    print("1. 输入ASN号码")
+    print("2. 输入具体的DN42节点信息")
+    print("3. 返回主菜单")
+
+    node_info = {'basicinfo': {}}
+
+    while True:
+        sub_choice = input("输入数字以选择功能: ")
+
+        if sub_choice == '1':
+            asn_num = input("请输入你的ASN号码: ")
+            node_info['basicinfo']['asnnum'] = asn_num
+            print("ASN号码已保存.")
+        elif sub_choice == '2':
+            node_name = input("请输入你的DN42节点名称: ")
+            dn42_ip = input("请输入你的DN42 IP: ")
+            wireguard_key = input("请输入你的WireGuard私钥: ")
+
+            node_info[node_name] = {
+                'dn42ip': dn42_ip,
+                'wireguardkey': wireguard_key
+            }
+            print(f"{node_name} 的节点信息已保存.")
+        elif sub_choice == '3':
+            break
+        else:
+            print("无效的选择，请重新输入。")
+
+    with open('node.yaml', 'w') as yaml_file:
+        yaml.dump(node_info, yaml_file, default_flow_style=False)
+
+    print("节点信息已保存到 'node.yaml'.")
+
 def main():
     while True:
-        print("\n选择一个功能:")
+        # 获取本地 Git 仓库的最新 commit hash，并在主菜单中显示
+        commit_hash = get_git_commit_hash()
+
+        print(f"\nDN42 quickshell by sam\n版本号: {commit_hash}")
+        print("\n请选择功能:")
         print("1. 更新roa")
         print("2. 创建新的BGP会话（仅v4）")
-        print("3. 退出")
+        print("3. 保存你的节点信息")
+        print("4. 退出")
 
         choice = input("输入数字以选择功能: ")
 
@@ -70,6 +124,8 @@ def main():
         elif choice == '2':
             create_new_bgp_session()
         elif choice == '3':
+            save_node_info()
+        elif choice == '4':
             print("程序结束。")
             break
         else:
@@ -83,4 +139,3 @@ def main():
 
 if __name__ == "__main__":
     main()
-
